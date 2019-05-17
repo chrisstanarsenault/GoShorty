64a612a7c680819c8ca085c5da4fcff9d763cffa
Chris Arsenault <chrisstanarsenault@gmail.com>
2019-05-16 22:00:48 -0400

    added missing staged files for ShortURL redirect

--- express_server.js
+++ express_server.js
@@ -1,5 +1,7 @@
 let express = require("express");
 let app = express();
+let bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 let PORT = 8080; //default port 8080;
 
 let urlDatabase = {
@@ -7,18 +9,46 @@ let urlDatabase = {
   "9sm5xK": "https://www.google.com"
 };
 
+let generateRandomString = () => {
+  let result = '';
+  let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+  let charactersLength = characters.length;
+  for (let i = 0; i < 6; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+};
+
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.send("Hello!");
+  res.send(generateRandomString());
 });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n")
+app.post("/urls", (req, res) => {
+  let randomShortURL = generateRandomString();
+  console.log(req.body); // Log the POST request body to the console
+  urlDatabase[randomShortURL] = req.body.longURL;
+  res.redirect(`/urls/${randomShortURL}`);
+})
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  res.render("urls_show", templateVars);
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  let longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
 });
 
 app.listen(PORT, () => {
