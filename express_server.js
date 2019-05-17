cf0ca4cff13d68aad0f71b6a32d0525a679dee11
Chris Arsenault <chrisstanarsenault@gmail.com>
2019-05-17 02:22:46 -0400

    added ESLint.  Added delete buttons for URLS

--- express_server.js
+++ express_server.js
@@ -1,18 +1,20 @@
-let express = require("express");
-let app = express();
-let bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}));
-let PORT = 8080; //default port 8080;
+const express = require("express");
 
-let urlDatabase = {
-  "b2xVn2": "https://www.lighthouselabs.ca",
-  "9sm5xK": "https://www.google.com"
+const app = express();
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+const PORT = 8080; // default port 8080;
+
+const urlDatabase = {
+  b2xVn2: "https://www.lighthouselabs.ca",
+  "9sm5xK": "https://www.google.com",
 };
 
-let generateRandomString = () => {
+const generateRandomString = () => {
   let result = '';
-  let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
-  let charactersLength = characters.length;
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+  const charactersLength = characters.length;
   for (let i = 0; i < 6; i++) {
     result += characters.charAt(Math.floor(Math.random() * charactersLength));
   }
@@ -26,28 +28,33 @@ app.get("/", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase };
+  const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
 app.post("/urls", (req, res) => {
-  let randomShortURL = generateRandomString();
+  const randomShortURL = generateRandomString();
   console.log(req.body); // Log the POST request body to the console
   urlDatabase[randomShortURL] = req.body.longURL;
   res.redirect(`/urls/${randomShortURL}`);
-})
+});
 
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
 
+app.post("/urls/:shortURL/delete", (req, res) => {
+  delete urlDatabase[req.params.shortURL];
+  res.redirect("/urls");
+});
+
 app.get("/u/:shortURL", (req, res) => {
-  let longURL = urlDatabase[req.params.shortURL];
+  const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
 });
 
